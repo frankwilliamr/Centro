212b00c195523077fdda64a61e8b634bd949803b
Frank William <frankwilliamr@gmail.com>
2024-09-17 13:42:11 -0300

    Subindo para o repositorio

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+
+import SignIn from './pages/sign-in/SignIn';
+import DashBoard from './pages/dashboard/Dashboard';
+
+export default function App() {
+  return (
+    <DashBoard/>
+  );
+}
---
+++ src/pages/dashboard/components/MenuContent.jsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Stack from '@mui/material/Stack';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import AnalyticsRoundedIcon from '@mui/icons-material/AnalyticsRounded';
+import PeopleRoundedIcon from '@mui/icons-material/PeopleRounded';
+import AssignmentRoundedIcon from '@mui/icons-material/AssignmentRounded';
+
+
+const mainListItems = [
+  { text: 'Analise Geral', icon: <HomeRoundedIcon /> },
+  { text: 'Prontuarios', icon: <AnalyticsRoundedIcon /> },
+  { text: 'Cadastro de Internos', icon: <PeopleRoundedIcon /> },
+  
+];
+
+
+
+export default function MenuContent() {
+  return (
+    <Stack sx={{ flexGrow: 1, p: 1, justifyContent: 'space-between' }}>
+      <List dense>
+        {mainListItems.map((item, index) => (
+          <ListItem key={index} disablePadding sx={{ display: 'block' }}>
+            <ListItemButton selected={index === 0}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+
+     
+    </Stack>
+  );
+}
